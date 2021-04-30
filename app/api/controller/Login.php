53848a21a6994e990323ee18378634d806bad6b4
liangchenyang <23426945@qq.com>
2021-05-01 02:19:15 +0800

    新增小程序接口路由定义文件

---
+++ app/api/controller/Login.php
@@ -0,0 +1,15 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\api\controller;
+
+class Login
+{
+    /**
+     * 登陆接口
+     */
+    public function index()
+    {
+        echo '登陆接口 success';
+    }
+}
