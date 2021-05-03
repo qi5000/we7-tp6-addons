7c7134eee8e37e58609b893b67275e60d7f8ebe9
holykinh <23426945@qq.com>
2021-05-03 16:18:38 +0800

    修改api应用容器定义文件

--- app/api/provider.php
+++ app/api/provider.php
@@ -3,8 +3,6 @@
 // 容器Provider定义文件
 
 return [
-    // EasyWeChat 功能封装
-    'EasyWeChat' => app\api\lib\EasyWeChat::class,
     // 自定义异常处理机制
     'think\exception\Handle' => Exception\ApiExceptionHandle::class,
 ];
