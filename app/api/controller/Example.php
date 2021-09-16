4ee9cd16cfece95a05e7033444c95ddbe6a7ad79
liang <23426945@qq.com>
2021-09-16 23:03:39 +0800

    订阅消息

--- app/api/controller/Example.php
+++ app/api/controller/Example.php
@@ -4,22 +4,37 @@ declare(strict_types=1);
 
 namespace app\api\controller;
 
-use app\api\logic\Subscribe;
+use app\common\logic\Subscribe;
+use app\common\logic\Payment as LogicPayment;
 
 /**
  * 使用示例
  */
 class Example
 {
+    public $openid = 'olCRt5W0cNgEh953SJBz20rio1fA';
+
     /**
      * 订阅消息
      */
     public function subscribe()
     {
-        // 辰风沐阳小程序
-        $openid = 'oNiWa5CB9VFKgr-TqssteGoieibY';
         // 发送订阅消息
-        $data = Subscribe::demo($openid, ['id' => 1, 'uid' => 6]);
+        // $data = Subscribe::demo($this->openid, ['id' => 1, 'uid' => 6]);
+        $data = Subscribe::demo($this->openid);
         return json($data);
+    }
+
+    /**
+     * 获取支付参数
+     */
+    public function payment()
+    {
+        $data = [
+            'total_fee' => 0.01,
+            'openid'    => $this->openid,
+        ];
+        $result = LogicPayment::create($data);
+        halt($result);
     }
 }
